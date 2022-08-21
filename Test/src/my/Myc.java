b09513cf6af9aa3f03a44fff78e4a09b1c04d74c
Shovan <Shovan@192.168.100.3>
2022-08-21 20:08:28 +0530

    new

---
+++ Test/src/my/Myc.java
@@ -0,0 +1,13 @@
+package my;
+
+public class Myc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellow world");
+		int a = 2;
+		System.out.println(a*a);
+
+	}
+
+}
